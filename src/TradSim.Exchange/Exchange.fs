0fbf4561de13d0977ac79cd521f55095e5b3b50b
Sotirios Mantziaris <s.mantziaris@live.com>
2016-07-27 22:23:57 +0300

    added order price and book and tests

--- src/TradSim.Exchange/Exchange.fs
+++ src/TradSim.Exchange/Exchange.fs
@@ -1,6 +1,7 @@
 ﻿module TradSim.Exchange
 
 open System
+open System.Collections.Generic
 
 type TradeDirection = Buy = 0 | Sell = 1
 type OrderStatus = Pending = 0 | PartiallyFilled = 1 | FullyFilled = 2 | OverFilled = 3
@@ -21,12 +22,55 @@ let setOrderStatus order =
     elif order.Quantity = order.OriginalQuantity then { order with Status = OrderStatus.FullyFilled }    
     else { order with Status = OrderStatus.OverFilled }
 
-type OrderPriceEntry = {
+type OrderQuantity = {
     Quantity: int
     Orders: Order list
 }
 
-let appendToPriceEntry entry order=
+let appendOrderToOrderQuantity entry order=
     match entry with
     | None -> { Quantity = order.OriginalQuantity; Orders = [order]}  
     | Some(e) -> {e with Quantity = e.Quantity + order.OriginalQuantity; Orders= List.append e.Orders [order]}
+
+type OrderPrice = {
+    Price:decimal
+    Buy: OrderQuantity
+    Sell: OrderQuantity 
+}
+
+type OrderBook = {
+    Symbol: Dictionary<string,OrderPrice list>
+}
+
+let createOrderPrice order =
+    match order.Direction with
+    | TradeDirection.Buy  -> { Price=order.Price; Buy = appendOrderToOrderQuantity None order ; Sell = { Quantity=0; Orders=[]} }
+    | TradeDirection.Sell -> { Price=order.Price; Buy = { Quantity=0; Orders=[]} ; Sell = appendOrderToOrderQuantity None order  }
+    | _                   -> raise <| new ArgumentOutOfRangeException("Direction",order.Direction,"Direction is not supported!")
+
+let matchOrderPrice price orderPrice  =
+    orderPrice.Price = price
+
+let matchOrderPriceList (prices:OrderPrice list) price =
+      let filtered = List.filter (matchOrderPrice price) prices
+      match List.length filtered with
+      | 0 -> None
+      | 1 -> Some (filtered.Item 0)
+      | _ -> raise <| new Exception("Only one order price should exist!")
+
+// processOrder
+//  if book does not contain symbol then addOrderToOrderBook
+//  if it contains symbol processSymbolOrder
+
+// let processOrder book (order:Order) =
+
+//     if book.Symbol.ContainsKey(order.Symbol) then
+//         match matchOrderPrices (book.Symbol.Item order.Symbol) order with
+//         | None book.Symbol.[order.Symbol] <- prices 
+//         | Some(p)
+
+//         let prices = processOrderPrices (book.Symbol.Item order.Symbol) order
+        
+//     else
+//         book.Symbol.Add(order.Symbol,[createOrderPrice order])    
+
--- test/TradSim.Exchange.Tests.Unit/ExchangeTests.fs
+++ test/TradSim.Exchange.Tests.Unit/ExchangeTests.fs
@@ -1,6 +1,7 @@
 ﻿module TradSim.ExchangeTests
 
 open System
+open System.Collections.Generic
 open Xunit
 open TradSim.Exchange
 
@@ -19,8 +20,59 @@ let ``Set Order Status Tests`` quantity expectedStatus =
 let ``Append multiple orders to empty order price entry`` () =
     let order1 = { Id=Guid.NewGuid() ; Symbol="TT"; Price=10.0m; Quantity = 10; OriginalQuantity = 10; Direction=TradeDirection.Buy; Status=OrderStatus.FullyFilled }
     let order2 = { Id=Guid.NewGuid() ; Symbol="TT"; Price=10.0m; Quantity = 10; OriginalQuantity = 15; Direction=TradeDirection.Buy; Status=OrderStatus.FullyFilled }
-    let entry = appendToPriceEntry (Some (appendToPriceEntry None order1)) order2
+    let entry = appendOrderToOrderQuantity (Some (appendOrderToOrderQuantity None order1)) order2
 
     Assert.Equal(25, entry.Quantity)
     Assert.Equal(order1, entry.Orders.Item 0)
     Assert.Equal(order2, entry.Orders.Item 1)
+
+[<Theory>]
+[<InlineData(TradeDirection.Buy)>]
+[<InlineData(TradeDirection.Sell)>]
+let ``Create OrderPrice`` direction =
+    let order = { Id=Guid.NewGuid() ; Symbol="TT"; Price=10.0m; Quantity = 10; OriginalQuantity = 10; Direction=direction; Status=OrderStatus.FullyFilled }
+    let orderPrice = createOrderPrice order
+
+    Assert.Equal(10.0m, orderPrice.Price)
+    if direction = TradeDirection.Sell then 
+        Assert.Equal(10,orderPrice.Sell.Quantity)
+        Assert.Equal(order,orderPrice.Sell.Orders.Item 0)
+    else
+        Assert.Equal(10,orderPrice.Buy.Quantity)
+        Assert.Equal(order,orderPrice.Buy.Orders.Item 0)
+
+[<Fact>]
+let ``Create OrderPrice with invalid direction throws`` () =
+    let order = { Id=Guid.NewGuid() ; Symbol="TT"; Price=10.0m; Quantity = 10; OriginalQuantity = 10; Direction= enum<TradeDirection> 10; Status=OrderStatus.FullyFilled }
+
+    Assert.Throws<ArgumentOutOfRangeException>(fun () -> createOrderPrice order |> ignore) |> ignore
+    
+
+[<Theory>]
+[<InlineData(10.0,true)>]
+[<InlineData(11.0,false)>]
+let ``Match order price`` price expectedMatch  =
+    let order = { Id=Guid.NewGuid() ; Symbol="TT"; Price=price; Quantity = 10; OriginalQuantity = 10; Direction= TradeDirection.Buy ; Status=OrderStatus.FullyFilled }
+    let orderPrice = { createOrderPrice order with Price=10.0m}
+
+    let actualMatch = matchOrderPrice order.Price orderPrice
+
+    Assert.Equal(expectedMatch , actualMatch)
+
+[<Fact>]
+let ``Match order price list throws on duplicate results`` ()  =
+    let order = { Id=Guid.NewGuid() ; Symbol="TT"; Price=10.0m; Quantity = 10; OriginalQuantity = 10; Direction= TradeDirection.Buy ; Status=OrderStatus.FullyFilled }
+    let list = [createOrderPrice order;createOrderPrice order]
+
+    Assert.Throws<Exception>(fun () -> matchOrderPriceList list order.Price |> ignore) |> ignore
+
+[<Theory>]
+[<InlineData(10.0,true)>]
+[<InlineData(12.0,false)>]
+let ``Match order price list`` price (expectedMatch:bool)  =
+    let order = { Id=Guid.NewGuid() ; Symbol="TT"; Price=price; Quantity = 10; OriginalQuantity = 10; Direction= TradeDirection.Buy ; Status=OrderStatus.FullyFilled }
+    let list = [createOrderPrice {order with Price=10.0m};createOrderPrice {order with Price=11.0m}]
+
+    match matchOrderPriceList list order.Price with
+    | None -> Assert.False(expectedMatch)
+    | Some(p) -> Assert.True(expectedMatch)
